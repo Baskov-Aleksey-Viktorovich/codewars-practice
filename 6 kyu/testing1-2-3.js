a9ac46221a5e6a1e35cce47908cb968ce8f8b21f
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2023-07-17 16:25:59 +0300

    add two variant answer)

--- 6 kyu/testing1-2-3.js
+++ 6 kyu/testing1-2-3.js
@@ -1,13 +1,17 @@
-
-
+//first variant
 let  number=function(array){
-    let res = [];
-    if(array.length <= 0){
-      return [];
-    }
-    for(let i = 0; i < array.length; i++){
-            res.push(`${i}: ${array[i]}`);
-            console.log(array[i], i);
-        }
-        return res;
-  };
+ return array.length <= 0 ? [] : array.map((c, i) => `${i + 1}: ${c}`);
+};
+
+// two variant
+//  let  number=function(array){
+//      let res = [];
+//      if(array.length <= 0){
+//        return [];
+//    }
+//    for(let i = 0; i < array.length; i++){
+//            res.push(`${i}: ${array[i]}`);
+//            console.log(array[i], i);
+//        }
+//        return res;
+//  };
